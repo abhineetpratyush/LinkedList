159585d587ad419f052c4dc593ab202bb4ec0f33
abhineetpratyush <abhineetpratyush1998@gmail.com>
2020-10-13 21:11:59 +0530

    [abhineet] add : ability to create linked list by appending values

--- src/main/java/com/capgemini/linkedlist/MyLinkedList.java
+++ src/main/java/com/capgemini/linkedlist/MyLinkedList.java
@@ -23,6 +23,17 @@ public class MyLinkedList {
 			this.head.setNext(tempNode);
 		}
 	}	
+	
+	public void append(INode newNode) {
+		if(this.head == null)
+			this.head = newNode;
+		if(this.tail == null)
+			this.tail = newNode;
+		else {
+			this.tail.setNext(newNode);
+			this.tail = newNode;
+		}
+	}	
 
 	public void printMyNodes() {
 		StringBuffer myNodes = new StringBuffer("My Nodes: ");
--- src/test/java/com/capgemini/linkedlist/MyLinkedListTest.java
+++ src/test/java/com/capgemini/linkedlist/MyLinkedListTest.java
@@ -17,4 +17,18 @@ public class MyLinkedListTest {
 		boolean result = myLinkedList.head.equals(myThirdNode) && myLinkedList.head.getNext().equals(mySecondNode) && myLinkedList.tail.equals(myFirstNode);
 		Assert.assertTrue(result);
 	}
+	
+	@Test
+	public void given3Numbers_WhenAddedToLinkedList_ShouldBeAddedToEnd() {
+		MyNode<Integer> myFirstNode = new MyNode<Integer>(56);
+		MyNode<Integer> mySecondNode = new MyNode<Integer>(30);
+		MyNode<Integer> myThirdNode = new MyNode<Integer>(70);
+		MyLinkedList myLinkedList = new MyLinkedList();
+		myLinkedList.append(myFirstNode);
+		myLinkedList.append(mySecondNode);
+		myLinkedList.append(myThirdNode);
+		myLinkedList.printMyNodes();
+		boolean result = myLinkedList.head.equals(myFirstNode) && myLinkedList.head.getNext().equals(mySecondNode) && myLinkedList.tail.equals(myThirdNode);
+		Assert.assertTrue(result);
+	}
 }
