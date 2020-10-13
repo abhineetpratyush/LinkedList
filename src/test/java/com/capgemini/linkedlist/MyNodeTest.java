506f416f53fcfaf0f708cdd674ed24daca2f7b71
abhineetpratyush <abhineetpratyush1998@gmail.com>
2020-10-13 19:56:36 +0530

    [abhineet] add : add MyNode.java and MyNodeTest.java to create 3 nodes and link them together

---
+++ src/test/java/com/capgemini/linkedlist/MyNodeTest.java
@@ -0,0 +1,17 @@
+package com.capgemini.linkedlist;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyNodeTest {
+	@Test
+	public void given3Numbers_WhenLinked_ShouldPassLinkedListTest() {
+		MyNode<Integer> myFirstNode = new MyNode<Integer>(56);
+		MyNode<Integer> mySecondNode = new MyNode<Integer>(30);
+		MyNode<Integer> myThirdNode = new MyNode<Integer>(70);    	
+		myFirstNode.setNext(mySecondNode);
+		mySecondNode.setNext(myThirdNode);
+		boolean result = myFirstNode.getNext().equals(mySecondNode) && mySecondNode.getNext().equals(myThirdNode);
+		Assert.assertTrue(result);
+	}
+}
